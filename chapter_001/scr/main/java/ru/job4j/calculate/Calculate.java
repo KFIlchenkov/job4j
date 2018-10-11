5ffc2b1fde12405b543d06ef53ca64f55ba946b5
Konstantin Filchenkov <filchenkov86@gmail.com>
2018-10-11 21:14:08 +0700

    1.3. JavaDoc. Code convention [4087]

--- chapter_001/scr/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/scr/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate;
+/**
+*Calculate
+*
+@author Konstantin Filchenkov (filchenkov86@gmail.com)
+*/
 	
 public class Calculate{
+	/**
+	*Main.
+	@param args - args.
+	*/
 		
 	public static void main(String[] args){
 			System.out.println("Hello World");
