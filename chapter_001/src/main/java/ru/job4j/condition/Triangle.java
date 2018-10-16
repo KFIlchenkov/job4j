37c4460bec4c26d0c037a8135ced641e566727b4
Konstantin Filchenkov <filchenkov86@gmail.com>
2018-10-16 07:00:08 +0700

    4.3. Вычисление площади треугольни [9461]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,49 @@
+package ru.job4j.condition;
+
+/**
+ * Triangle
+ * @author Konstantin Filchenkov/
+ * @version $Id$.
+ * @since 15.10.2018.
+ */
+
+public class Triangle {
+    private Point a;
+    private Point b;
+    private Point c;
+
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    /**
+     * Method to calcylate period
+     * @param ab -
+     * @param bc -
+     * @param ca -
+     * @return period
+     */
+
+    public double period(double ab, double bc, double ca) {
+        return (ab + bc + ca) / 2;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = this.a.distanceTo(this.b);
+        double bc = this.b.distanceTo(this.c);
+        double ca = this.c.distanceTo(this.a);
+        double p = this.period(ab, bc, ca);
+        if (this.exist(ab, ca)) {
+           rsl = Math.sqrt(p * (p - ab) * (p - bc) * (p - ca));
+        }
+        return rsl;
+    }
+
+    private boolean exist(double ab, double ca) {
+        return ab != 0 & ca != 0;
+        }
+
+}
